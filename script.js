00c967b5420ae0877ea6c95e15cd605b2db02cdc
Gideon Ogordi <gideonogordi@gmail.com>
2024-09-11 11:38:11 +0100

    again

--- script.js
+++ script.js
@@ -1,67 +1,23 @@
-/* 
-🌟 APP: Make Netflix
-
-Here we have the Netflix app but it's up to you to make it work by pulling all the movies using an API!
-
-Create a fetchMovies() function that will make a dynamic API call to what you need 👇
-========================================
-
-- fetchMovies()
-
-** fetchMovies takes in an URL, a div id or class from the HTML, and a path (poster or backdrop)
-
-
-
-These are the 3 main functions and their URL'S you must create  👇
-========================================
-
-- getOriginals()
-  * URL : 'https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
-
-- getTrendingNow()
-  * URL : 'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
-
-- getTopRated()
-  * URL : 'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
-
-
-** These functions will provide the URL you need to fetch() movies of that genere **
-
-These are all the DIV ID's you're gonna need access to 👇
-========================================================  
-#1 CLASS 👉 'original__movies' = Div that holds Netflix Originals
-#2 ID 👉 'trending' = Div that holds trending Movies
-#3 ID 👉 'top_rated' = Div that holds top rated Movies
-*/
-
-// Call the main functions the page is loaded
 window.onload = () => {
     getOriginals()
     getTrendingNow()
     getTopRated()
   }
   
-  // ** Helper function that makes dynamic API calls **
   async function fetchMovies(url, dom_element, path_type) {
-    // Use Fetch with the url passed down 
     response = await fetch(url)
     if(response.ok){
         data = await response.json()
     }else{
         throw `Something went wrong`
     }
-    // console.log(data)
     showMovies(data,dom_element, path_type)
-    // Within Fetch get the response and call showMovies() with the data , dom_element, and path type
   }
   
-  //  ** Function that displays the movies to the DOM **
   const showMovies = (movies, dom_element, path_type) => {
     
-    // Create a variable that grabs id or class
   var movieL=document.getElementById(dom_element)
   
-    // Loop through object
   for(let movie of movies.results){
     var img = document.createElement("img")
     img.setAttribute('data-id',movie.id)
@@ -72,25 +28,20 @@ window.onload = () => {
     movieL.appendChild(img)
     }  
   }
-  // ** Function that fetches Netflix Originals **
    function getOriginals() {
   url='https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
   fetchMovies(url,'original__movies','poster_path')
   }
-  // ** Function that fetches Trending Movies **
    function getTrendingNow() {
     url='https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045'
     fetchMovies(url,'trending','poster_path')
   }
-  // ** Function that fetches Top Rated Movies **
    function getTopRated() {
     url='https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'
     fetchMovies(url,'top_rated','poster_path')
   }
   
-  // ** BONUS **
   
-  // ** Fetches URL provided and returns response.json()
   async function getMovieTrailer(id) {
     var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`
     return await fetch(url).then(response => {
@@ -132,9 +83,7 @@ window.onload = () => {
       setTrailer(youtubeTrailers)
     })
   
-    // open modal
     $('#trailerModal').modal('show')
-    // we need to call the api with the ID
   }
   
   
